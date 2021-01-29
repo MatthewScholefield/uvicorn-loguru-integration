334c4c71546a0f1d5187c1c126e81a4752b484f8
Matthew D. Scholefield <matthew331199@gmail.com>
2021-01-29 12:33:46 -0600

    Increment version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='uvicorn-loguru-integration',
-    version='0.1.0',
+    version='0.2.0',
     description='Code to integrate uvicorn.run with Loguru logging',
     url='https://github.com/MatthewScholefield/uvicorn-loguru-integration',
     author='Matthew D. Scholefield',
